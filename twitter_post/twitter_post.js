5cda83f3f9dd811273496736f862d408fcb9d9c5
juampy <juampy72@gmail.com>
2013-01-04 01:24:19 +0100

    Issue #1853794 by grobot and juampy: Added Please consider restoring twitter.lib.php to the Twitter module.
    Reviewed Twitter Post submodule.

--- twitter_post/twitter_post.js
+++ twitter_post/twitter_post.js
@@ -2,32 +2,28 @@
  * Attach handlers to toggle the twitter message field and inform the number
  * of characters remaining to achieve the max length
  */
-(function ($) {
-  Drupal.behaviors.twitter_post = {
-    attach: function (context, settings) {
-      $("#twitter-textfield", context).keyup(function() {
-        var charsLeft = (140 - $(this).val().length);
-        var descDiv = $(this).next();
-        $(descDiv).html("<strong>" + charsLeft + "</strong> characters remaining");
-        if (charsLeft < 0) {
-          $(descDiv).addClass("negative");
-        } else {
-          $(descDiv).removeClass("negative");
-        }
-      });
-
-      if (!$("#twitter-toggle").attr("checked")) {
-        $(".form-item-twitter-status").hide();
-      }
-
-      $("#twitter-toggle").bind("click", function() {
-        if ($("#twitter-toggle").attr("checked")) {
-          $(".form-item-twitter-status").show();
-        }
-        else {
-          $(".form-item-twitter-status").hide();
-        }
-      });
+Drupal.behaviors.twitter_post = function (context) {
+  $("#twitter-textfield", context).keyup(function() {
+    var charsLeft = (140 - $(this).val().length);
+    var descDiv = $(this).next();
+    $(descDiv).html("<strong>" + charsLeft + "</strong> characters remaining");
+    if (charsLeft < 0) {
+      $(descDiv).addClass("negative");
+    } else {
+      $(descDiv).removeClass("negative");
     }
-  };
-}(jQuery));
+  });
+
+  if (!$("#twitter-toggle").attr("checked")) {
+    $(".form-item-twitter-status").hide();
+  }
+
+  $("#twitter-toggle").bind("click", function() {
+    if ($("#twitter-toggle").attr("checked")) {
+      $(".form-item-twitter-status").show();
+    }
+    else {
+      $(".form-item-twitter-status").hide();
+    }
+  });
+};
