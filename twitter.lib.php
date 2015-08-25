9248e24471b0fc87c80aa46f4912a65b74974342
Damien McKenna <damien@mc-kenna.com>
2015-08-24 21:07:08 -0400

    Issue #2239041 by DamienMcKenna: Replaced all http:// URLs with https:// ones.

--- twitter.lib.php
+++ twitter.lib.php
@@ -195,12 +195,15 @@ class Twitter {
     return drupal_http_request($url, array('headers' => $headers, 'method' => $method, 'data' => $data));
   }
 
+  /**
+   * @see https://www.drupal.org/node/985544
+   */
   protected function parse_response($response) {
-    // http://drupal.org/node/985544 - json_decode large integer issue
     $length = strlen(PHP_INT_MAX);
     $response = preg_replace('/"(id|in_reply_to_status_id|in_reply_to_user_id)":(\d{' . $length . ',})/', '"\1":"\2"', $response);
     return json_decode($response, TRUE);
   }
+
   /**
    * Creates an API endpoint URL.
    *
